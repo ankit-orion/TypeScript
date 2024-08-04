9e975c13f7801ee567a6fa6d2ba4a78c80eb30e3
Ankit Mishra <72105760+ankit-orion@users.noreply.github.com>
2024-08-04 12:43:00 +0530

    TypeScript code comments explaining its benefits

---
+++ main.ts
@@ -0,0 +1,11 @@
+// typescript is a super set of javascript that allows us to write code in a more structured way
+// it is a statically typed language, which means that we have to define the type of the variables
+// it allows us to write code that is more readable and maintainable
+// it is compiled to javascript, so it can run in any browser
+// it is a superset of javascript, so it can run in any browser
+// typescript is like writing javascript with a precise syntax
+// type script is all about type safety
+// type safety is a way to prevent errors in our code
+// ex in javascript we can add 2 + '2' and it will return 22 as a string which is not what we want
+// in typescript we can't do that, we have to define the type of the variables
+// ex null + 2 will return 2 in javascript, but in typescript it will return an error
